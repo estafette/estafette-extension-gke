53d434e09faedc5f3415bcf2f71332707e9347b8
Victoria Lopez Clemente <vlopez@travix.com>
2019-06-26 10:21:23 +0200

    added slack notifications logic

--- main.go
+++ main.go
@@ -1,8 +1,10 @@
 package main
 
 import (
+	"bytes"
 	"encoding/json"
 	"fmt"
+	"io"
 	"io/ioutil"
 	"log"
 	"net/http"
@@ -190,12 +192,35 @@ func main() {
 			//rollback canary
 		}
 		// deploy-stable
+
+		sendNotifications("succeeded")
+
 		paramsCopy.Action = "rollback-canary"
 		templateDataRollbackCanary, tmplRollbackCanary := generateKubernetesYaml(paramsCopy)
 		applyKubernetesYaml(paramsCopy, templateDataRollbackCanary, tmplRollbackCanary)
 	} else {
 		templateData, tmpl := generateKubernetesYaml(params)
 		applyKubernetesYaml(params, templateData, tmpl)
+	}
+}
+
+func sendNotifications(status string) {
+	var message = ""
+	switch status {
+	case "succeeded":
+		message = "good"
+	case "failed":
+		message = "Your last deployment generate too many errors... rolling back"
+	}
+
+	//send slack notification
+	var webhookURL = "https://hooks.slack.com/services/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"
+	var channels [1]string
+
+	for i := range channels {
+		err := SendSlackNotification(channels[i], "To many errors!", message, status, webhookURL)
+		log.Fatal(err)
+
 	}
 }
 
@@ -656,4 +681,99 @@ func httpRequestHeader(method, url string, headers map[string]string, responseHe
 	}
 
 	return response.Header.Get(responseHeader)
+}
+func SendSlackNotification(channel, title, message, status, webhookURL string) (err error) {
+
+	var requestBody io.Reader
+
+	color := ""
+	switch status {
+	case "succeeded":
+		color = "good"
+	case "failed":
+		color = "danger"
+	}
+
+	slackMessageBody := SlackMessageBody{
+		Channel:  channel,
+		Username: "Mary Poppins",
+		Attachments: []SlackMessageAttachment{
+			SlackMessageAttachment{
+				Fallback:   message,
+				Title:      title,
+				Text:       message,
+				Color:      color,
+				MarkdownIn: []string{"text"},
+			},
+		},
+	}
+
+	data, err := json.Marshal(slackMessageBody)
+	if err != nil {
+		log.Printf("Failed marshalling SlackMessageBody: %v. Error: %v", slackMessageBody, err)
+		return
+	}
+	requestBody = bytes.NewReader(data)
+
+	client := pester.New()
+	client.MaxRetries = 3
+	client.Backoff = pester.ExponentialJitterBackoff
+	client.KeepLog = true
+	request, err := http.NewRequest("POST", webhookURL, requestBody)
+	if err != nil {
+		log.Printf("Failed creating http client: %v", err)
+		return
+	}
+
+	// add headers
+	request.Header.Add("Content-type", "application/json")
+
+	// perform actual request
+	response, err := client.Do(request)
+	if err != nil {
+		log.Printf("Failed performing http request to Slack: %v", err)
+		return
+	}
+
+	defer response.Body.Close()
+
+	return
+}
+
+// SlackMessageBody represents the body to send to slack
+type SlackMessageBody struct {
+	Channel     string                   `json:"channel"`
+	Username    string                   `json:"username"`
+	Text        string                   `json:"text,omitempty"`
+	Attachments []SlackMessageAttachment `json:"attachments,omitempty"`
+}
+
+// SlackMessageAttachment represents attachments to a slack message
+type SlackMessageAttachment struct {
+	Fallback   string               `json:"fallback"`
+	Color      string               `json:"color,omitempty"`
+	Pretext    string               `json:"pretext,omitempty"`
+	AuthorName string               `json:"author_name,omitempty"`
+	AuthorLink string               `json:"author_link,omitempty"`
+	AuthorIcon string               `json:"author_icon,omitempty"`
+	Title      string               `json:"title,omitempty"`
+	TitleLink  string               `json:"title_link,omitempty"`
+	Text       string               `json:"text,omitempty"`
+	ImageURL   string               `json:"image_url,omitempty"`
+	ThumbURL   string               `json:"thumb_url,omitempty"`
+	Footer     string               `json:"footer,omitempty"`
+	FooterIcon string               `json:"footer_icon,omitempty"`
+	Ts         int                  `json:"ts,omitempty"`
+	MarkdownIn []string             `json:"mrkdwn_in,omitempty"`
+	Actions    []SlackMessageAction `json:"actions,omitempty"`
+}
+
+// SlackMessageAction represents an action (button)
+type SlackMessageAction struct {
+	Type    string `json:"type,omitempty"`
+	Name    string `json:"name,omitempty"`
+	Text    string `json:"text,omitempty"`
+	URL     string `json:"url,omitempty"`
+	Style   string `json:"style,omitempty"`
+	Confirm string `json:"confirm,omitempty"`
 }
