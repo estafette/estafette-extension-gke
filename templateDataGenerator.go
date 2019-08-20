cd75137f5d84f86aaed8275360a24c6bd28356a6
Jorrit Salverda <jsalverda@travix.com>
2019-08-20 10:42:37 +0200

    set probabilistic sampling with 0.1 likelihood and track=canary tags for canary releases

--- templateDataGenerator.go
+++ templateDataGenerator.go
@@ -115,8 +115,15 @@ func generateTemplateData(params Params, currentReplicas int, gitSource, gitOwne
 
 	// set tracing service name
 	data.Container.EnvironmentVariables = addEnvironmentVariableIfNotSet(data.Container.EnvironmentVariables, "JAEGER_SERVICE_NAME", params.App)
-	data.Container.EnvironmentVariables = addEnvironmentVariableIfNotSet(data.Container.EnvironmentVariables, "JAEGER_SAMPLER_TYPE", "remote")
-	data.Container.EnvironmentVariables = addEnvironmentVariableIfNotSet(data.Container.EnvironmentVariables, "JAEGER_SAMPLER_PARAM", "0.001")
+
+	if params.Action == "deploy-canary" {
+		data.Container.EnvironmentVariables = addEnvironmentVariableIfNotSet(data.Container.EnvironmentVariables, "JAEGER_SAMPLER_TYPE", "probabilistic")
+		data.Container.EnvironmentVariables = addEnvironmentVariableIfNotSet(data.Container.EnvironmentVariables, "JAEGER_SAMPLER_PARAM", "0.1")
+		data.Container.EnvironmentVariables = addEnvironmentVariableIfNotSet(data.Container.EnvironmentVariables, "JAEGER_TAGS", "track=canary")
+	} else {
+		data.Container.EnvironmentVariables = addEnvironmentVariableIfNotSet(data.Container.EnvironmentVariables, "JAEGER_SAMPLER_TYPE", "remote")
+		data.Container.EnvironmentVariables = addEnvironmentVariableIfNotSet(data.Container.EnvironmentVariables, "JAEGER_SAMPLER_PARAM", "0.001")
+	}
 
 	data.HasOpenrestySidecar = false
 	for _, sidecarParams := range params.Sidecars {
