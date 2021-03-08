5da8a121dcb90f469c70b411d96252cc695e7f41
Jorrit Salverda <jsalverda@travix.com>
2021-03-08 10:25:12 +0100

    document remaining fields; cloud sql proxy 1.20.2

--- api/params.go
+++ api/params.go
@@ -573,7 +573,7 @@ func (p *Params) SetDefaults(gitSource, gitOwner, gitName, appLabel, buildVersio
 		p.DefaultESPv2SidecarImage = "gcr.io/endpoints-release/endpoints-runtime:2.25.0"
 	}
 	if p.DefaultCloudSQLProxySidecarImage == "" {
-		p.DefaultCloudSQLProxySidecarImage = "eu.gcr.io/cloudsql-docker/gce-proxy:1.17"
+		p.DefaultCloudSQLProxySidecarImage = "eu.gcr.io/cloudsql-docker/gce-proxy:1.20.2"
 	}
 
 	for i := range p.Sidecars {
