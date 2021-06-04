77776155159a25061693919d6a3cb3286089708e
Jorrit Salverda <JorritSalverda@users.noreply.github.com>
2021-06-04 09:34:30 +0200

    update eu.gcr.io/cloudsql-docker/gce-proxy default to 1.23.0 (#68)

--- api/params.go
+++ api/params.go
@@ -602,7 +602,7 @@ func (p *Params) SetDefaults(gitSource, gitOwner, gitName, appLabel, buildVersio
 		p.DefaultESPv2SidecarImage = "gcr.io/endpoints-release/endpoints-runtime:2.25.0"
 	}
 	if p.DefaultCloudSQLProxySidecarImage == "" {
-		p.DefaultCloudSQLProxySidecarImage = "eu.gcr.io/cloudsql-docker/gce-proxy:1.21.0"
+		p.DefaultCloudSQLProxySidecarImage = "eu.gcr.io/cloudsql-docker/gce-proxy:1.23.0"
 	}
 
 	for i := range p.Sidecars {
