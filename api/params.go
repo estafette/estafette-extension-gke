75e4ba903d9755795b7c17a575f51dbc9b31e7c1
Jorrit Salverda <jsalverda@travix.com>
2021-07-16 09:53:04 +0200

    update defaults to esp 1.57.0, 2.29.1 and cloud sql proxy 1.23.1

--- api/params.go
+++ api/params.go
@@ -599,13 +599,13 @@ func (p *Params) SetDefaults(gitSource, gitOwner, gitName, appLabel, buildVersio
 		p.DefaultOpenrestySidecarImage = "estafette/openresty-sidecar@sha256:2aa9f2c8c3f506e0f6cc70871701b5ac81aa0f12e8574c7b8213e4d0379d2ddd"
 	}
 	if p.DefaultESPSidecarImage == "" {
-		p.DefaultESPSidecarImage = "gcr.io/endpoints-release/endpoints-runtime:1.56.0"
+		p.DefaultESPSidecarImage = "gcr.io/endpoints-release/endpoints-runtime:1.57.0"
 	}
 	if p.DefaultESPv2SidecarImage == "" {
-		p.DefaultESPv2SidecarImage = "gcr.io/endpoints-release/endpoints-runtime:2.25.0"
+		p.DefaultESPv2SidecarImage = "gcr.io/endpoints-release/endpoints-runtime:2.29.1"
 	}
 	if p.DefaultCloudSQLProxySidecarImage == "" {
-		p.DefaultCloudSQLProxySidecarImage = "eu.gcr.io/cloudsql-docker/gce-proxy:1.23.0"
+		p.DefaultCloudSQLProxySidecarImage = "eu.gcr.io/cloudsql-docker/gce-proxy:1.23.1"
 	}
 
 	for i := range p.Sidecars {
